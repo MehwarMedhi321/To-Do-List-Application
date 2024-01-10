a4d44bb46dce29058051d5e3cc0f9412ef726bf4
Mahwar Zaidi <Mahwar@gmail.com>
2024-01-10 21:20:52 +0530

    create the function of Enter key and make a button to change the text COLOR

--- To-doList.js
+++ To-doList.js
@@ -1,5 +1,6 @@
-let inputBox=document.getElementById("input-Box");
-let listContainer=document.getElementById("list-Container");
+
+let inputBox = document.getElementById("input-Box");
+let listContainer = document.getElementById("list-Container");
 
 function addTask(){
     if (inputBox.value === '') {
@@ -16,20 +17,53 @@ function addTask(){
     inputBox.value= "";
     saveData();
 }
-listContainer.addEventListener("click", function (e) {
+
+
+inputBox.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter' && inputBox.value !== ''){
+if (inputBox.value === '') {
+    alert("Add & Write something")
+}
+else{
+    let li =document.createElement("li");
+    li.innerHTML=inputBox.value;
+    listContainer.appendChild(li);
+    let span =document.createElement("span");
+    span.innerHTML="\u00d7";
+    li.appendChild(span);
+}
+inputBox.value= "";
+saveData();
+}
+}),
+
+
+listContainer.addEventListener("click",function (e) {
     if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
-        saveData();
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
     }
-    else if (e.target.tagName === "SPAN") {
-        e.target.parentElement.remove();
-    }
-},false);
+  },
+  false
+);
+
 function saveData() {
-    localStorage.setItem("data",listContainer.innerHTML);
-};
+  localStorage.setItem("data", listContainer.innerHTML);
+}
 
 function showTask() {
-    listContainer.innerHTML= localStorage.getItem("data")
+  listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
+
+let textChange = document.getElementById("text-Color");
+let colorText = ["YELLOW","black"];
+textChange.addEventListener("click",()=>{
+    let randomColor = colorText[Math.floor(Math.random()* colorText.length)];
+    listContainer.style.color = randomColor; 
+    listContainer.style.fontWeight = "bold";
+})
+
